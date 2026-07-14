c809127a07a6bf3cf0cd4e745dc2313c98eccbf5
NID <nid@example.com>
2026-07-14 19:25:58 +0200

    fix: update main.jsx to use explicit './store/index.js' import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode, lazy, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import { store, persistor } from './store/store'
+import { store, persistor } from './store/index.js'
 import './index.css'
 import App from './App'
 
